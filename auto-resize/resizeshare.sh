179b35fecb9792d44cb043763195701157ac0651
Nightshade <stephenj@inexistence.org>
2018-10-22 18:21:32 -0400

    Update auto-resize/resizeshare.sh
    Comment what the script is doing.

--- auto-resize/resizeshare.sh
+++ auto-resize/resizeshare.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Set needed variable (start cylinder for partition)
+PART_START=$(fdisk -l /dev/mmcblk0 | grep 0p3 | cut -c 26-30)
+# Stop everything that is using the share mount
 /etc/init.d/S02splash stop
 /etc/init.d/S25lircd stop
 /etc/init.d/S31emulationstation stop
@@ -6,30 +9,28 @@
 /etc/init.d/S91smb stop
 /etc/init.d/S92virtualgamepads stop
 /etc/init.d/S94manager stop
+# Unmount share to be safe
 umount /dev/mmcblk0p3
+# Force check share partition for errors and auto fix them
 fsck /dev/mmcblk0p3 -yf
-PART_NUM=3
-#PART_START=$(parted /dev/mmcblk0 -ms unit s p | grep "^${PART_NUM}" | cut -f 2 -d:)
-PART_START=$(fdisk -l /dev/mmcblk0 | grep 0p3 | cut -c 26-30)
+# Reparition the share to the new size
 fdisk /dev/mmcblk0 <<EOF
 p
 d
 $PART_NUM
 n
 p
-$PART_NUM
+3
 $PART_START
 
 p
 w
 EOF
-fsck /dev/mmcblk0p3 -yf
+# Reload the partition map in kernel space
 partprobe
+# Resize the partition to the new size
 /usr/sbin/resize2fs /dev/mmcblk0p3
+# Force check share partition for errors and auto fix them
 fsck /dev/mmcblk0p3 -yf
-mount -o remount,rw /
-rm /etc/init.d/S01resize2fs_once
-mount -o remount,ro /
-#shutdown -r now
-
-### end ###
+# We are done reboot to apply
+shutdown -r now
